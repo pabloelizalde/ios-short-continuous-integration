6f5287d0cb3deedef3e7f6a19255ae634816f382
Pablo Elizalde <pelizcar@gmail.com>
2017-01-19 12:05:44 +0100

    Add toRoman unit tests

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -2,8 +2,8 @@
 //  RomanNumeralCalculatorUITests.swift
 //  RomanNumeralCalculatorUITests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by Pablo Elizalde Carvajal on 19/01/2017.
+//  Copyright © 2017 buddybuild. All rights reserved.
 //
 
 import XCTest
